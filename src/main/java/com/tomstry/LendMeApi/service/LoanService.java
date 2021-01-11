7ecf05bf682b7afbf2331fa1ba7e4b75aa0431d2
thomasnaslund <mokowkingn123>
2021-01-11 17:26:07 +0100

    Added overlap check for loan interval #2

--- src/main/java/com/tomstry/LendMeApi/service/LoanService.java
+++ src/main/java/com/tomstry/LendMeApi/service/LoanService.java
@@ -14,8 +14,7 @@ import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.Period;
 import java.time.ZonedDateTime;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 
 @Service
 public class LoanService {
@@ -29,16 +28,21 @@ public class LoanService {
         return loan;
     }
 
+    //TODO: Should split this to multiple general functions
     public Loan addLoan(Loan loan) {
 
             for (ItemLoan itemloan : loan.getItems()) {
-
                 // is item is attached to other loans?
-
                 if (!itemloan.getItem().getLoans().isEmpty()) {
-
                     //Check if dates overlap
-
+                    Set<ItemLoan> loans = itemloan.getItem().getLoans();
+                    for (ItemLoan existingLoan : loans) {
+                        ZonedDateTime start = existingLoan.getLoan().getStart();
+                        ZonedDateTime end = existingLoan.getLoan().getEnd();
+                        if (hasOverlap(start, end, loan.getStart(), loan.getEnd())) {
+                            // TODO: Should Log overlap here
+                        }
+                    }
                 }
             }
 
